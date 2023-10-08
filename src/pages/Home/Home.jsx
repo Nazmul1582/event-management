64fb7532b5d49fa711485ab5f0f0c0e5fc71273a
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-08 20:04:35 +0600

    fake data created and added in publice folder then loaded data in home route

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,10 @@
+import { useLoaderData } from "react-router-dom";
 import Services from "../../components/Services";
 import Banner from "./Banner";
 
 const Home = () => {
+    const allServices = useLoaderData()
+    console.log(allServices);
     return (
         <>
             <Banner />
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -14,6 +14,7 @@ const router = createBrowserRouter([
         children: [
             {
                 path: "/",
+                loader: () => fetch("services.json"),
                 element: <Home />
             },
             {

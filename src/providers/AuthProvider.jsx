713a7397ea643e3fac4bdb9e5b05fd8145600193
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-10-09 08:47:11 +0600

    installed firebase and setup firebase config and put in environment variable

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -4,9 +4,11 @@ import PropTypes from 'prop-types';
 export const AuthContext = createContext(null);
 
 
-const authInfo = { name: "Md. Nazmul Hasan"}
 
 const AuthProvider = ({children}) => {
+
+    const authInfo = { name: "Md. Nazmul Hasan"}
+
     return (
         <AuthContext.Provider value = {authInfo}>
             {children}
